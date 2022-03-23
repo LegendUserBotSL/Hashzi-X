8173275e501d4ab77892612c219eb95c0ef8a76f
LegendUserBotSL <102130077+LegendUserBotSL@users.noreply.github.com>
2022-03-23 11:22:43 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,64 +1,69 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2020 Sadew.
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+
+WhatsLegend - Sadew
 */
 
 const chalk = require('chalk');
 const { WAConnection, MessageType } = require('@adiwajshing/baileys');
 const fs = require('fs');
+
 async function whatsAsena() {
-  const conn = new WAConnection();
-  conn.logger.level = 'warn';
-  conn.version = [2, 2126, 14]
+	const conn = new WAConnection();
+	conn.logger.level = 'warn';
+	conn.version = [3, 3234, 9]
 
-  conn.on('connecting', async () => {
-    console.log(`${chalk.green.bold('Amazone Alexa')}${chalk.green.bold('Team')}
-${chalk.white.italic('amazone String code recipient')}
-${chalk.blue.bold('ℹ️  Connecting alexa... Please wait.')}`);
-  });
+	conn.on('connecting', async () => {
+		console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Legend')}
+${chalk.white.italic('LegendString Kodu Alıcı')}
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
+	});
 
-  conn.on('open', async () => {
-    console.log(
-      chalk.green.bold('Alexa QR Code: '),
-      'NEUTRO;;;' +
-      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-        'base64'
-      )
-    );
-    await conn.sendMessage(
-      conn.user.jid,
-      'NEUTRO;;;' +
-      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-        'base64'
-      ),
-      MessageType.text
-    );
-    if (conn.user.jid.startsWith('91')) {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ Amazone Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
-        MessageType.text
-      );
-    } else {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ Amazone Successfully Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
-        MessageType.text
-      );
-    }
-    console.log(
-      chalk.green.bold(
-        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
-      ),
-      chalk.green.bold(
-        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
-      )
-    );
-    process.exit(0);
-  });
+	conn.on('open', async () => {
+		console.log(
+			chalk.green.bold('Legend QR Code: '),
+			'SADEW;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				)
+		);
+		await conn.sendMessage(
+			conn.user.jid,
+			'SADEW;;;' +
+				Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+					'base64'
+				),
+			MessageType.text
+		);
+		if (conn.user.jid.startsWith('90')) {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ කරුණාකර මෙම කේතය කිසිවෙකු සමඟ බෙදා නොගන්න ' + conn.user.name + '* ⚠️',
+				MessageType.text
+			);
+		} else {
+			await conn.sendMessage(
+				conn.user.jid,
+				'*⚠️ Please Do Not Share This Code With Anyone ' +
+					conn.user.name +
+					'* ⚠️',
+				MessageType.text
+			);
+		}
+		console.log(
+			chalk.green.bold(
+				"ඔබට පණිවිඩය පිටපත් කිරීමට නොහැකි නම්, කරුණාකර WHATSAPP පරීක්ෂා කරන්න.  QR කේතය ඔබේම අංකයට යවන ලදි!\n"
+			),
+			chalk.green.bold(
+				'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+			)
+		);
+		process.exit(0);
+	});
 
-  await conn.connect();
+	await conn.connect();
 }
 
 whatsAsena();
